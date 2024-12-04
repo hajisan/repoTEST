2a1ebf9ab94c7d6ba0fa4b9b3bb080faf99287ac
Joakim R. S. <jose0001@stud.kea.dk>
2024-12-04 12:37:49 +0100

    createJoakim
    createJoakim() added to psvm

--- nyprojekt/src/Main.java
+++ nyprojekt/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        createJoakim();
+    }
+
+    public static void createJoakim() {
+        Joakim joakim = new Joakim();
+        System.out.println("Mit navn er " + joakim.getName());
     }
 }
